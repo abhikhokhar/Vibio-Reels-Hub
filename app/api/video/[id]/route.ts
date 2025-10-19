7b000a000ef7946dd2d552591f79cd0a307cfadd
Abhi Khokhar <abhikhokhar2004@gmail.com>
2025-10-19 15:00:55 +0530

    Deployment Bug fix

--- app/api/video/[id]/route.ts
+++ app/api/video/[id]/route.ts
@@ -4,12 +4,13 @@ import { connectToDatabase } from "@/lib/db";
 
 export async function GET(
   req: Request,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> } 
 ) {
   try {
+    const { id } = await context.params;
     await connectToDatabase();
-    const video = await Video.findById(params.id);
-    
+
+    const video = await Video.findById(id);
 
     if (!video) {
       return NextResponse.json({ error: "Video not found" }, { status: 404 });
